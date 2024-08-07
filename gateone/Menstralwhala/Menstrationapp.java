d8211fc099dab18d661364a96bbbc851f6445a42
mysterymartial <bolasax16@gmail.com>
2024-08-07 09:30:36 +0100

    upadate

--- gateone/Menstralwhala/Menstrationapp.java
+++ gateone/Menstralwhala/Menstrationapp.java
@@ -1,3 +1,37 @@
 public class Menstrationapp{
 
-	public static String [] feedback(int avergeMenstrationCycle, int startOfPeriod, int periodOfFlow){ 
+	public int [] hostDate( int month, int day, int year){ 
+		int [] daysOfMonth = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+		int []monthOfTheEntry = new int[2];
+		final int myArrayLength = daysOfMonth.length;
+	
+			int firstMonthSize = daysOfMonth[month];
+			monthOfTheEntry[0] = firstMonthSize;
+			int secondMonthSize = daysOfMonth[month + 1];
+			monthOfTheEntry[1] = secondMonthSize;
+
+			return monthOfTheEntry;
+
+}
+ 	public int [] calendar(int [] daysOfTwoMonth){
+		int number = 1;
+		int sum = daysOfTwoMonth[0] + daysOfTwoMonth[1];
+		int [] totalDays = new int [sum];
+		for(int counter =0; counter < totalDays.length; counter++){
+			if(counter != daysOfTwoMonth[0]){
+		
+				totalDays[counter] = number;
+				number++;
+		}else{
+			number = 1;
+			
+				totalDays[counter] = number;
+					number++;
+		}
+
+		}
+	return totalDays;
+
+}
+
+}			
---
+++ gateone/Menstralwhala/MenstrationappTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class MenstrationappTest{
+
+	@Test
+	public void testToCheckDateEntered() {
+		Menstrationapp app = new Menstrationapp();
+		int [] result = app.hostDate(5,16,2010);
+		int [] calendarMonth = {31,30};
+		assertArrayEquals(calendarMonth,result);
+
+}
+	@Test
+	public void checkingCalenderLength(){
+
+	Menstrationapp app = new Menstrationapp();
+		int[] daysOfTwoMonth= {31,30};
+		int[] calenderOfTwoMonths={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
+		int [] result = app.calendar(daysOfTwoMonth);
+	assertArrayEquals(calenderOfTwoMonths,result);
+}
+
+	@Test
+	public void checkingFlowDates(){
+	Menstrationapp app = new Menstrationapp();
+	int[] calenderOfTwoMonths={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
+int [] feedback = {3,4,5,6,7};
+int result = app.flowDate(3,5,calenderOfTwoMonths);
+assertArrayEquals(feedback,result);
+}
+
+	
+}
